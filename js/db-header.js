85a8d40d37a70751e4fc8644558264b7d6504e4a
untypequicode <untypequicode@gmail.com>
2024-04-03 13:54:27 +0200

    Addition of the first header content system.

---
+++ js/db-header.js
@@ -0,0 +1,12 @@
+const db_header = [
+    {
+        text: "Accueil",
+        url: "https://untypequicode.github.io/style.css",
+    },
+];
+
+const logo = {
+    type: "image",
+    content: "images/favicon.svg",
+    link: "https://untypequicode.github.io/style.css",
+}
---
+++ js/header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector(".header");
+    var logo = document.createElement("a");
+    var logoImage = document.createElement("img");
+    var menuIcon = document.createElement("div");
+    var navbar = document.createElement("nav");
+    var activeNav = document.createElement("span");
+});
