7ac171b8161dea974cd2f8d5421824e99dae46cc
untypequicode <untypequicode@gmail.com>
2024-04-04 08:14:09 +0200

    Update on the JavaScript header generation now functioning.

--- js/header.js
+++ js/header.js
@@ -57,3 +57,49 @@ document.addEventListener("DOMContentLoaded", function () {
         menuIcon.classList.toggle("bx-x");
     };
 });
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    if (sections.length > 0) {
+        sections.forEach((sec) => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 100;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute("id");
+
+            if (top >= offset && top < offset + height) {
+                if (navLinks.length > 0) {
+                    navLinks.forEach((links) => {
+                        links.classList.remove("active");
+                        if (document.querySelector("header nav a[href*=" + id + "]") !== null) {
+                            document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+                        }
+                    });
+                }
+                sec.classList.add("show-animate");
+            }
+            else {
+                sec.classList.remove("show-animate");
+            }
+        });
+    }
+    let header = document.querySelector(".header");
+
+    if (header !== null) {
+        header.classList.toggle("sticky", window.scrollY > 100);
+    }
+
+    menuIcon = document.getElementById("menu-icon");
+    navbar = document.querySelector(".navbar");
+
+    if (menuIcon.classList.contains("bx-x")) {
+        menuIcon.classList.remove("bx-x");
+    }
+    navbar.classList.remove("active");
+
+    let footer = document.querySelector("footer");
+
+    footer.classList.toggle("show-animate", this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
