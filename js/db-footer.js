7f9951926c7926b6baefdcc5ba66f451c3145b70
untypequicode <untypequicode@gmail.com>
2024-05-26 02:17:42 +0200

    Adding legal disclaimers.

--- js/db-footer.js
+++ js/db-footer.js
@@ -1,6 +1,6 @@
 const db_footer = {
     left: {
         content: 'Copyright \u00A9 2024 Style.css | Tous droits réservés.',
-        link: 'https://untypequicode.github.io/style.css/'
+        link: 'mentions-legales'
     }
 }
